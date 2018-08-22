f49417d553491235c663dcf70585d47c0b72934b
Sean Kozer <sean.kozer@gmail.com>
2018-08-22 11:33:29 +1000

    es6 formatting

--- warhol.js
+++ warhol.js
@@ -2,152 +2,130 @@
 // http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/#demo-simple
 // https://hacks.mozilla.org/2009/06/pop-art-video/
 
-const numBoards = 6;
-const context = new Array(numBoards);
+const numBoards = 6
+const context = new Array(numBoards)
 const colours = {
-	dark: [
-		'#201f7c',
-		'#ec027b',
-		'#e9061d',
-		'#7d4292',
-		'#212025',
-		'#ed6c03',
-	],
-	light: [
-		'#8cb21d',
-		'#fefb00',
-		'#f693c3',
-		'#7dbbf4',
-		'#fcfafb',
-		'#fcfea8',
-	],
-	background: [
-		'#fefa08',
-		'#0279ea',
-		'#281f80',
-		'#ee8609',
-		'#e30f21',
-		'#90b70b',
-	]
+  dark: ['#201f7c', '#ec027b', '#e9061d', '#7d4292', '#212025', '#ed6c03'],
+  light: ['#8cb21d', '#fefb00', '#f693c3', '#7dbbf4', '#fcfafb', '#fcfea8'],
+  background: ['#fefa08', '#0279ea', '#281f80', '#ee8609', '#e30f21', '#90b70b'],
 }
 
 // Holds the current state of the entire artwork
-let clickX = new Array();
-let clickY = new Array();
-let clickDrag = new Array();
-let clickShade = new Array();
-let clickRadius = new Array();
+let clickX = new Array()
+let clickY = new Array()
+let clickDrag = new Array()
+let clickShade = new Array()
+let clickRadius = new Array()
 
 // Holds the current state
-let curShade = 'dark';
-let curRadius = 5;
-let paint;
+let curShade = 'dark'
+let curRadius = 5
+let paint = false
 
-const prepareCanvas = (canvasId) => {
-	const selector = $('canvas[data-id="' + canvasId + '"]');
-	const ctx = selector[0].getContext("2d");
-	context[canvasId] = ctx;
+const prepareCanvas = canvasId => {
+  const selector = $('canvas[data-id="' + canvasId + '"]')
+  const ctx = selector[0].getContext('2d')
+  context[canvasId] = ctx
 
-	// Set the background colour
-	ctx.fillStyle = colours.background[canvasId];
-	ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  // Set the background colour
+  ctx.fillStyle = colours.background[canvasId]
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
-	// Add mouse events
-	selector.mousedown(function(e) {
-		// Mouse down location
-		const mouseX = e.pageX - this.offsetLeft;
-		const mouseY = e.pageY - this.offsetTop;
+  // Add mouse events
+  selector.mousedown(e => {
+    // Mouse down location
+    const mouseX = e.pageX - e.target.offsetLeft
+    const mouseY = e.pageY - e.target.offsetTop
 
-		paint = true;
-		addClick(mouseX, mouseY, false);
-		redraw();
-	});
+    paint = true
+    addClick(mouseX, mouseY, false)
+    redraw()
+  })
 
-	selector.mousemove(function(e) {
-		if (paint) {
-			addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
-			redraw();
-		}
-	});
+  selector.mousemove(e => {
+    if (paint) {
+      addClick(e.pageX - e.target.offsetLeft, e.pageY - e.target.offsetTop, true)
+      redraw()
+    }
+  })
 
-	selector.mouseup(function(e) {
-		paint = false;
-	  redraw();
-	});
+  selector.mouseup(() => {
+    paint = false
+    redraw()
+  })
 
-	selector.mouseleave(function(e) {
-		paint = false;
-	});
+  selector.mouseleave(() => {
+    paint = false
+  })
 }
 
-function addClick(x, y, dragging) {
-	clickX.push(x);
-	clickY.push(y);
-	clickDrag.push(dragging);
-	clickShade.push(curShade);
-	clickRadius.push(curRadius);
+const addClick = (x, y, dragging) => {
+  clickX.push(x)
+  clickY.push(y)
+  clickDrag.push(dragging)
+  clickShade.push(curShade)
+  clickRadius.push(curRadius)
 }
 
-function clearCanvas() {
-	context.forEach((ctx) => {
-		ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+const clearCanvas = () => {
+  context.forEach(ctx => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height)
 
-		// Set the background colour
-		ctx.fillStyle = colours.background[ctx.canvas.dataset.id];
-		ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-	})
+    // Set the background colour
+    ctx.fillStyle = colours.background[ctx.canvas.dataset.id]
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  })
 }
 
-function redraw() {
-	clearCanvas();
+const redraw = () => {
+  clearCanvas()
 
-	context.forEach((ctx) => {
-		ctx.lineJoin = "round";
-	})
+  context.forEach(ctx => {
+    ctx.lineJoin = 'round'
+  })
 
-	for (let i=0; i < clickX.length; i++) {
-		context.forEach((ctx) => {
-			const canvasId = ctx.canvas.dataset.id;
-			const shade = clickShade[i];
-			const strokeColour = colours[shade][canvasId];
+  for (let i = 0; i < clickX.length; i++) {
+    context.forEach(ctx => {
+      const canvasId = ctx.canvas.dataset.id
+      const shade = clickShade[i]
+      const strokeColour = colours[shade][canvasId]
 
-			ctx.beginPath();
-			if (clickDrag[i] && i) {
-				ctx.moveTo(clickX[i-1], clickY[i-1]);
-			} else {
-				ctx.moveTo(clickX[i]-1, clickY[i]);
-			}
-			ctx.lineTo(clickX[i], clickY[i]);
-			ctx.closePath();
-			ctx.strokeStyle = strokeColour;
-			ctx.lineWidth = clickRadius[i];
-			ctx.stroke();
-		})
-
-	}
+      ctx.beginPath()
+      if (clickDrag[i] && i) {
+        ctx.moveTo(clickX[i - 1], clickY[i - 1])
+      } else {
+        ctx.moveTo(clickX[i] - 1, clickY[i])
+      }
+      ctx.lineTo(clickX[i], clickY[i])
+      ctx.closePath()
+      ctx.strokeStyle = strokeColour
+      ctx.lineWidth = clickRadius[i]
+      ctx.stroke()
+    })
+  }
 }
 
-function main () {
-	for (let i=0; i < numBoards; i++) {
-		prepareCanvas(i);
-	}
+const main = () => {
+  for (let i = 0; i < numBoards; i++) {
+    prepareCanvas(i)
+  }
 
-	$('#shades button').click(e => {
-		curShade = e.target.dataset.shade;
-	});
+  $('#shades button').click(e => {
+    curShade = e.target.dataset.shade
+  })
 
-	$('#sizes button').click(e => {
-		curRadius = e.target.dataset.radius;
-	});
+  $('#sizes button').click(e => {
+    curRadius = e.target.dataset.radius
+  })
 
-	$('#clearCanvas').click(() => {
-		clickX = new Array();
-		clickY = new Array();
-		clickDrag = new Array();
-		clickShade = new Array();
-		clickRadius = new Array();
-		clearCanvas();
-	});
+  $('#clearCanvas').click(() => {
+    clickX = new Array()
+    clickY = new Array()
+    clickDrag = new Array()
+    clickShade = new Array()
+    clickRadius = new Array()
+    clearCanvas()
+  })
 }
 
-main();
+main()
