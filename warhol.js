3499e16e1ebfcc69ac66df7a73905a002029e44e
Sean Kozer <sean.kozer@gmail.com>
2018-08-24 18:13:04 +1000

    Add touch event listeners

--- warhol.js
+++ warhol.js
@@ -1,6 +1,7 @@
 // Inspired by:
 // http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/#demo-simple
 // https://hacks.mozilla.org/2009/06/pop-art-video/
+// http://bencentra.com/code/2014/12/05/html5-canvas-touch-events.html
 
 const numBoards = 6
 const context = new Array(numBoards)
@@ -30,6 +31,50 @@ const prepareCanvas = canvasId => {
   // Set the background colour
   ctx.fillStyle = colours.background[canvasId]
   ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+
+  // Set up touch events for mobile, etc
+  ctx.canvas.addEventListener(
+    'touchstart',
+    function(e) {
+      mousePos = getTouchPos(ctx.canvas, e)
+      var touch = e.touches[0]
+      var mouseEvent = new MouseEvent('mousedown', {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      })
+      ctx.canvas.dispatchEvent(mouseEvent)
+    },
+    false
+  )
+  ctx.canvas.addEventListener(
+    'touchend',
+    function(e) {
+      var mouseEvent = new MouseEvent('mouseup', {})
+      ctx.canvas.dispatchEvent(mouseEvent)
+    },
+    false
+  )
+  ctx.canvas.addEventListener(
+    'touchmove',
+    function(e) {
+      var touch = e.touches[0]
+      var mouseEvent = new MouseEvent('mousemove', {
+        clientX: touch.clientX,
+        clientY: touch.clientY,
+      })
+      ctx.canvas.dispatchEvent(mouseEvent)
+    },
+    false
+  )
+
+  // Get the position of a touch relative to the canvas
+  function getTouchPos(canvasDom, touchEvent) {
+    var rect = canvasDom.getBoundingClientRect()
+    return {
+      x: touchEvent.touches[0].clientX - rect.left,
+      y: touchEvent.touches[0].clientY - rect.top,
+    }
+  }
 
   // Add mouse events
   selector.mousedown(e => {
